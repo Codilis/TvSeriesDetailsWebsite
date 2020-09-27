956b1443fffdd4e1fce18c5849834822f523c234
Palash <palash.j22@gmail.com>
2020-09-28 00:22:25 +0530

    updated class name for tv_series_name and update_type

--- main/models.py
+++ main/models.py
@@ -6,6 +6,6 @@ import random
 
 # Create your models here.
 class UserTvSeries(forms.Form):
-	tv_series_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'TV Series Name', 'class': 'form-control'}), label='')
-	update_type = forms.TypedChoiceField(choices=AVAILABLE_CHOICES, label='')
+	tv_series_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'TV Series Name', 'class': 'series-name'}), label='')
+	update_type = forms.TypedChoiceField(choices=AVAILABLE_CHOICES, widget=forms.Select(attrs={'placeholder': 'TV Series Name', 'class': 'series-update-type'}), label='')
 	date_added = forms.DateField(initial=datetime.now, widget=forms.TextInput(attrs={'hidden': 'hidden'}), label='')
