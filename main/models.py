781cf037d254f39578b49f214575f5cc33e82638
Palash <palash.j22@gmail.com>
2020-09-29 23:59:56 +0530

    Added Model class UserTvSeriesModel and used it in form class UserTvSeries

--- main/models.py
+++ main/models.py
@@ -5,7 +5,19 @@ from django import forms
 import random
 
 # Create your models here.
-class UserTvSeries(forms.Form):
-	tv_series_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'TV Series Name', 'class': 'series-name'}), label='')
-	update_type = forms.TypedChoiceField(choices=AVAILABLE_CHOICES, widget=forms.Select(attrs={'placeholder': 'TV Series Name', 'class': 'series-update-type'}), label='')
-	date_added = forms.DateField(initial=datetime.now, widget=forms.TextInput(attrs={'hidden': 'hidden'}), label='')
+class UserTvSeriesModel(models.Model):
+	tv_series_id = models.CharField(max_length=1023)
+	update_type = models.CharField(max_length=1023)
+	date_added = models.DateTimeField()
+	user = models.CharField(max_length=1023)
+
+
+class UserTvSeries(forms.ModelForm):
+	tv_series_id = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'IMDb ID', 'class': 'series-name'}), label='')
+	update_type = forms.TypedChoiceField(choices=AVAILABLE_CHOICES, widget=forms.Select(attrs={'class': 'series-update-type'}), label='')
+	date_added = forms.DateTimeField(initial=datetime.now, widget=forms.DateTimeInput(attrs={'hidden': 'hidden'}), label='')
+	user = forms.CharField(initial=datetime.now, widget=forms.TextInput(attrs={'hidden': 'hidden'}), label='')
+
+	class Meta:
+		model = UserTvSeriesModel
+		fields = '__all__'
