34b1f61ed656380c6b585bb104f5d9f88d663e04
Palash <palash.j22@gmail.com>
2020-09-29 23:58:56 +0530

    Changed Update Type to Update Duration

--- main/constants.py
+++ main/constants.py
@@ -1,1 +1,1 @@
-AVAILABLE_CHOICES = (("", "Update Type"), ("N", "No Updates"), ("M", "Monthly"), ("D", "Daily"), ("W", "Weekly"), ("H", "6 Months"), ("Y", "Yearly"),)
+AVAILABLE_CHOICES = (("", "Update Duration"), ("N", "No Updates"), ("M", "Monthly"), ("D", "Daily"), ("W", "Weekly"), ("H", "6 Months"), ("Y", "Yearly"),)
