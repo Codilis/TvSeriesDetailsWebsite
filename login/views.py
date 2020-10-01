08fd3f833f2bca645d676da4d4eb13dd11e56d08
Palash Jain <palash.j22@gmail.com>
2020-10-01 21:20:29 +0530

    Added View for logout Code to Check if user is logged in or not is also added along with redirects

--- login/views.py
+++ login/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
 from django.contrib import messages
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-from .models import LoginForm, CreateAccountForm
+from .models import LoginForm, CreateAccountForm, ChangePasswordForm
+from main.models import UserTvSeries
 
 
 # Create your views here.
 # Login View
 def loginuser(request):
+	if request.user.is_authenticated:
+		return HttpResponseRedirect('/main/')
 	login_page = loader.get_template('login/login.htm')
 	context = {}
 	context['form'] = LoginForm()
@@ -17,6 +23,8 @@ def loginuser(request):
 
 # Create Account
 def createaccount(request):
+	if request.user.is_authenticated:
+		return HttpResponseRedirect('/main/')
 	login_page = loader.get_template('login/createaccount.htm')
 	context = {}
 	if request.method == 'POST':
@@ -24,7 +32,10 @@ def createaccount(request):
 		if form.is_valid():
 			username = form.cleaned_data.get('email')
 			p = form.save()
+			password = form.cleaned_data.get('password1')
 			messages.success(request, f"New account created: {username}")
+			user = authenticate(username=username, password=password)
+			login(request, user)
 			return HttpResponseRedirect('/main/')
 		else:
 			for msg in form.error_messages:
@@ -38,18 +49,44 @@ def createaccount(request):
 
 # Forget Password
 def forgetpassword(request):
+	if request.user.is_authenticated:
+		return HttpResponseRedirect('/main/')
 	login_page = loader.get_template('login/forgetpassword.htm')
 	context = {}
 	return HttpResponse(login_page.render(context, request))
 
 # Change Password
+@login_required(login_url='/login/')
 def changepassword(request):
-	login_page = loader.get_template('login/changepassword.htm')
+	if not request.user.is_authenticated:
+		HttpResponseRedirect('/login/')
 	context = {}
+	context['username'] = request.user
+	if request.method == 'POST':
+		form = PasswordChangeForm(request.user, request.POST)
+		if form.is_valid():
+			user = form.save()
+			username = request.user
+			password = form.cleaned_data.get('new_password2')
+			update_session_auth_hash(request, user)  # Important!
+			messages.success(request, 'Your password was successfully updated!')
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			return HttpResponseRedirect('/changepassword/')
+		else:
+			for msg in form.error_messages:
+				if 'inactive' not in form.error_messages[msg]:
+					messages.error(request, f"{form.error_messages[msg]}")
+			context['form'] = ChangePasswordForm(request.user)
+	else:
+			context['form'] = ChangePasswordForm(request.user)
+	login_page = loader.get_template('login/changepassword.htm')
 	return HttpResponse(login_page.render(context, request))
 
 # Check Credentials
 def logincheck(request):
+	if request.user.is_authenticated:
+		return HttpResponseRedirect('/main/')
 	form = LoginForm(request=request, data=request.POST)
 	context = {}
 	if request.method != 'POST':
@@ -74,7 +111,6 @@ def logincheck(request):
 
 	if user.is_active:
 		login(request, user)
-		# messages.success(request, f"Logged in Successfully")
 		return HttpResponseRedirect('/main/')
 
 	for msg in form.error_messages:
@@ -82,3 +118,9 @@ def logincheck(request):
 			messages.error(request, f"{form.error_messages[msg]}")
 	context['form'] = form
 	return render(request = request, template_name = "login/login.htm", context={"form":form})
+
+
+def userlogout(request):
+	if request.user.is_authenticated:
+		logout(request)
+	return HttpResponseRedirect('/login/')
