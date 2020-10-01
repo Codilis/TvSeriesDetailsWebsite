9987480808b2e6d89d1b5e9fe3fd99e08587a4d3
Palash Jain <palash.j22@gmail.com>
2020-10-01 21:24:20 +0530

    Added Dictionary AVAILABLE_CHOICE_KEYS

--- main/constants.py
+++ main/constants.py
@@ -1,1 +1,2 @@
 AVAILABLE_CHOICES = (("", "Update Duration"), ("N", "No Updates"), ("M", "Monthly"), ("D", "Daily"), ("W", "Weekly"), ("H", "6 Months"), ("Y", "Yearly"),)
+AVAILABLE_CHOICES_KEYS = {"N":"No Updates", "M":"Monthly", "D":"Daily", "W":"Weekly", "H":"6 Months", "Y":"Yearly"}
