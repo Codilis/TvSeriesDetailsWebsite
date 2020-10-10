0f6a033344732f33380ec0b2c6507ba9d24c61ec
Palash Jain <palash.j22@gmail.com>
2020-10-10 21:34:32 +0530

    Added Static FIle Storage

--- TvSeriesDetails/settings.py
+++ TvSeriesDetails/settings.py
@@ -192,5 +192,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 django_heroku.settings(locals())
